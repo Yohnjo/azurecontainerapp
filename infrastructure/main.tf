b4e44ba0c6df6763b4f7ecb6cbab58e0f7fddc62
Yohnjo <obandoac@gmail.com>
2024-05-16 19:23:17 -0600

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,6 +8,7 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
+
 resource "azurerm_service_plan" "service_plan" {
   name                = "webapp-plan"
   resource_group_name = azurerm_resource_group.rg.name
