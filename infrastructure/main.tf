c771c33f4df07006d66cccb6c97cc8f829ffba72
Yohnjo <obandoac@gmail.com>
2024-05-16 22:55:28 -0600

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,9 +1,5 @@
 provider "azurerm" {
   features {}
-    subscription_id = 05cc9ded-341d-4fc8-86e2-374d537c5579
-    client_id       = 78ff5737-aea9-4d81-82b9-d08eb575b2ca
-    client_secret   = FGx8Q~UnjFx_CpK0wBkueqEwqg3gT6_dAERSNa63
-    tenant_id       = bfcf1d9d-93ea-43b1-b902-1daa68a64248
 }
 
 resource "azurerm_resource_group" "rg" {
